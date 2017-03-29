4eba14583ac8f7d243f487eafda7658c0c2f8156
Maricela <malvarezdrobush@gmail.com>
2017-03-28 19:19:32 -0500

    done

--- dist/js/moto.js
+++
@@ -1,8 +0,0 @@
-function Moto(){
-	this.litrosGastados = function(distancia){
-        var consumo = 21; 
-        var bencina = 673;
-		// retorna la cantidad de litros que gasta en una distancia
-        return (distancia/consumo)*bencina;
-	}
-}
